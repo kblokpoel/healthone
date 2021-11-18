cf397ed82007aef59adf2ca75e1403e6bb3534fb
302620548 <302620548@student.rocmondriaan.nl>
2021-11-18 10:28:49 +0100

    time

---
+++ htdocs/Templates/defaults/header.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="header-row row lead" >
+        <h1 class="display-2">Health<span class="text-warning">One</span></h1>
+<!--        <img src='/img/Logo-Health-One.png' width="100vw" height="auto"/>-->
+
+    </div>
+</header>
